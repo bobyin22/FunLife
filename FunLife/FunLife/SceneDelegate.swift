8c475f6ed9e68310df221f07c13506bba2b4f44e
bobyin <bobyin22@gmail.com>
2023-06-15 11:34:39 +0800

    yml cancel line length disable

--- FunLife/FunLife/SceneDelegate.swift
+++ FunLife/FunLife/SceneDelegate.swift
@@ -11,6 +11,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    // swiftlint:disable line_length
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -49,4 +51,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    // swiftlint:enable line_length
 }
