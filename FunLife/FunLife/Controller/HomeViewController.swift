3492f983296494235cf1a69ef9b0a123f7001a65
bobyin <bobyin22@gmail.com>
2023-06-15 14:35:54 +0800

    [feat] HomeVC setup tabBar icon

---
+++ FunLife/FunLife/Controller/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  FunLife
+//
+//  Created by 邱慧珊 on 2023/6/15.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {          //: BaseViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemOrange
+    }
+    
+}
