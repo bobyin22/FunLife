baa8397992425ca7cce0ee6fa70a1b3eb8fa3908
bobyin <bobyin22@gmail.com>
2023-06-15 15:23:28 +0800

    [feat] HomeVC setup TimerLabel

--- FunLife/FunLife/Controller/HomeViewController.swift
+++ FunLife/FunLife/Controller/HomeViewController.swift
@@ -9,20 +9,59 @@ import UIKit
 
 class HomeViewController: UIViewController {          //: BaseViewController
 
+    let circleView = UIView()           // 圓形
+    let circleTimerLabel = UILabel()    // 計時時間
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemOrange
+        view.backgroundColor = .systemGray
         
-        setNavigation()
+        setupNavigation()
+        setupCircleUI()
+        setupTimer()
     }
     
     // NavBar + 按鈕設置
-    func setNavigation() {
-        
+    func setupNavigation() {
         let settingSButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: nil)
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        navigationItem.rightBarButtonItems = [settingSButton, addButton]    // 兩個按鈕
+    }
+    
+    // 建立圓形View
+    func setupCircleUI() {
+        view.addSubview(circleView)
+        circleView.backgroundColor = .systemYellow
+        circleView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            circleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -200),
+            circleView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: -150),
+            //circle.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -75),
+            circleView.heightAnchor.constraint(equalToConstant: 300),
+            circleView.widthAnchor.constraint(equalToConstant: 300),
+        ])
         
-        navigationItem.rightBarButtonItems = [settingSButton, addButton]
+        circleView.layer.borderColor = UIColor.black.cgColor
+        circleView.layer.borderWidth = 2.0
+
+        // 圓半徑設為 寬的一半
+        circleView.layer.cornerRadius = 150
+        // 確保圓形圖不顯示超出邊界的部分
+        circleView.clipsToBounds = true
+        circleView.layer.masksToBounds = false
+    }
+    
+    // 建立倒數計時器
+    func setupTimer() {
+        view.addSubview(circleTimerLabel)
+        circleTimerLabel.text = "00:00:00"
+        circleTimerLabel.font = UIFont(name: "Helvetica", size: 50)
+        circleTimerLabel.backgroundColor = .systemRed
+        circleTimerLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            circleTimerLabel.topAnchor.constraint(equalTo: circleView.topAnchor, constant: 130),
+            circleTimerLabel.leadingAnchor.constraint(equalTo: circleView.centerXAnchor, constant: -95),
+        ])
     }
 }
