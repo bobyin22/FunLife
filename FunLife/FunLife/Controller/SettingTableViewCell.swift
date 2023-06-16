3c25d45b756a5a453cca8b02ce6b6da087e24c86
bobyin <bobyin22@gmail.com>
2023-06-16 08:13:13 +0800

    [feat] HomeVC push to SettingVC, SettingVC TableView create

---
+++ FunLife/FunLife/Controller/SettingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTableViewCell.swift
+//  FunLife
+//
+//  Created by 邱慧珊 on 2023/6/16.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
