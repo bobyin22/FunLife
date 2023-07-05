e1a1bbd40dbd8be2672e1d6f8f3bb8391c5cf6d4
bobyin <bobyin22@gmail.com>
2023-07-05 16:38:31 +0800

    [feat] pod install Crashlytics and Analytics

--- FunLife/FunLife/Controller/TabBar0/HomeViewController.swift
+++ FunLife/FunLife/Controller/TabBar0/HomeViewController.swift
@@ -50,7 +50,18 @@ class HomeViewController: UIViewController {
 
         // 6️⃣當作是自己
         addTaskVC.delegate = self
+        
+        let button = UIButton(type: .roundedRect)
+              button.frame = CGRect(x: 20, y: 250, width: 100, height: 30)
+              button.setTitle("Test Crash", for: [])
+              button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+              view.addSubview(button)
     }
+    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+          let numbers = [0]
+          let _ = numbers[1]
+      }
     
     // MARK: 讓每次返回本頁會顯示
     override func viewWillAppear(_ animated: Bool) {
